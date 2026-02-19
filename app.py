f7e87941770ef657373df8de4c82e7c184a8ea46
PinkPanther21 <moyeon2109@gmail.com>
2026-02-19 14:22:47 +0500

    corrections

--- app.py
+++ app.py
@@ -16,11 +16,8 @@ scaler = pickle.load(open("scaler.pkl", "rb"))
 
 # Load dataset just to get column names (optional)
 df = pd.read_csv("health_data.csv")
-# X_columns = df.drop("cardio", axis=1).columns
-X_columns = [
-    'age', 'gender', 'height', 'weight', 'ap_hi', 'ap_lo',
-    'cholesterol', 'gluc', 'smoke', 'alco', 'active'
-]
+X_columns = df.drop("cardio", axis=1).columns
+
 
 # -----------------------------
 # Streamlit Interface
